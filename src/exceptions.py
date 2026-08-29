fb8cb7bcb8a5a9c5ff9489c967c198786e398d61
monedales <le.moneda@outlook.com>
2026-08-29 00:59:00 -0300

    feat: add custom exception classes for error handling in the pipeline

---
+++ src/exceptions.py
@@ -0,0 +1,20 @@
+class PipelineError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class FrameReadError(PipelineError):
+    pass
+
+
+class InsufficientEventsError(PipelineError):
+    pass
+
+
+class MissingImageError(PipelineError):
+    pass
+
+
+class VideoDownloadError(PipelineError):
+    pass
